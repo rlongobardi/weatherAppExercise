3141627bb7eb06ad069b716958092e53b4aa2345
Rosario <rosario.longobardi@gmail.com>
2023-11-21 18:53:26 +0000

    Weather API Service

--- src/test/java/com/myweather/api/integration/WeatherControllerIntegrationTest.java
+++ src/test/java/com/myweather/api/integration/WeatherControllerIntegrationTest.java
@@ -1,13 +1,14 @@
 package com.myweather.api.integration;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WeatherControllerIntegrationTest {
@@ -15,18 +16,60 @@ public class WeatherControllerIntegrationTest {
     @LocalServerPort
     private int port;
 
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
     @Test
     public void testGetWeatherByCity() {
-        RestAssured.baseURI = "http://localhost:" + port;
-
-        Response response = RestAssured.given()
+        String cityName = "London";
+        given()
+                .contentType(ContentType.JSON)
                 .when()
-                .get("/weather/city/London")
+                .get("/weather/city/" + cityName)
                 .then()
                 .statusCode(200)
-                .extract()
-                .response();
+                .body("city", equalTo(cityName))
+                .body("temperature", notNullValue())
+                .body("humidity", notNullValue());
+    }
 
-        assertThat(response.jsonPath().getString("city"), is("London"));
+    @Test
+    public void testGetHistory() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/weather/history")
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0))
+                .body("history[0].city", notNullValue())
+                .body("history[0].temperature", notNullValue())
+                .body("history[0].humidity", notNullValue());
+    }
+
+    @Test
+    public void testGetWeatherByCityNotFound() {
+        String cityName = "NonExistentCity";
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/weather/city/" + cityName)
+                .then()
+                .statusCode(404)
+                .body("error", equalTo("City not found"));
+    }
+
+    @Test
+    public void testGetHistoryNotFound() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/weather/history/nonexistent")
+                .then()
+                .statusCode(404)
+                .body("error", equalTo("History not found"));
     }
 }
